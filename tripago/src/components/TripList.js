f9203170be1e6e02ee81b118da7d4b564bf75d52
2AM-official <zhaoxinglyu@icloud.com>
2022-11-21 16:04:31 -0800

    basic useEffect

---
+++ tripago/src/components/TripList.js
@@ -0,0 +1,44 @@
+import { useState, useEffect, useCallback } from 'react'
+
+// styles
+import './TripList.css'
+
+export default function TripList() {
+
+    const [trips, setTrips] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/trips')
+
+    const fetchTrips = useCallback(async () => {
+        const response = await fetch(url)
+        const json = await response.json()
+        setTrips(json)
+    }, [url])
+
+    useEffect(() => {
+        fetchTrips()
+    }, [fetchTrips])
+
+    console.log(trips)
+
+    return (
+        <div className="trip-list">
+            <h2>Trip List</h2>
+            <ul>
+                {trips.map(trip => (
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className='filters'>
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+                    European Trips
+                </button>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>
+                    All Trips
+                </button>
+            </div>
+        </div>
+    )
+}
