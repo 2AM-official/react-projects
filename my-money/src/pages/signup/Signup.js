d2aed891a2e6728f256cf04eba8e62f0ce2f5757
2AM-official <zhaoxinglyu@icloud.com>
2022-11-30 15:55:21 -0800

    signup hook

--- my-money/src/pages/signup/Signup.js
+++ my-money/src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 // styles
 import styles from './Signup.module.css'
 
@@ -6,10 +7,11 @@ export default function Signup() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [displayName, setDisplayName] = useState('')
+    const { signup, isPending, error }  = useSignup()
     
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email, password, displayName)
+        signup(email, password, displayName)
     }
     
   return (
@@ -39,7 +41,9 @@ export default function Signup() {
                 value={displayName}
             />
         </label>
-        <button className='btn'>Signup</button>
+        {!isPending && <button className='btn'>Signup</button>}
+        {isPending && <button className='btn' disabled>loading</button>}
+        {error && <p>{error}</p>}
     </form>
   )
 }
